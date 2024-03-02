04f6eade0859ba09b853c8663c70b8f8da7f0e2b
finnaib <shoaibwwe01@gmail.com>
2024-03-02 23:42:31 +0530

    pantah : update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,9 +3,9 @@ echo 'Starting to clone stuffs needed to build for pantah'
 # Device common
 echo 'Cloning common device tree'
 
-git clone https://github.com/Finnaib/android_device_google_gs201.git -b aosp device/google/gs201
-git clone https://github.com/Finnaib/android_device_google_gs101.git -b aosp device/google/gs101
-git clone https://github.com/Finnaib/android_device_google_gs-common.git -b lineage-21 device/google/gs-common
+git clone https://github.com/Finnaib/device_google_gs101.git -b aosp-14 device/google/gs201
+git clone https://github.com/Finnaib/device_google_gs201.git -b aosp-14 device/google/gs101
+git clone https://github.com/Finnaib/device_google_gs-common.git -b aosp-14 device/google/gs-common
 
 # Kernel
 echo 'Cloning kernel tree'
@@ -23,6 +23,7 @@ git clone https://github.com/Evolution-X-Devices/packages_apps_PixelParts.git -b
 echo 'Cloning bcr'
 rm -rf vendor/bcr && git clone --depth=1 https://github.com/kenway214/vendor_bcr.git -b main vendor/bcr
 
+#hardware/google
 rm -rf hardware/google/pixel && git clone https://github.com/LineageOS/android_hardware_google_pixel -b lineage-21.0 hardware/google/pixel
 
 rm -rf hardware/google/graphics/common && git clone https://github.com/LineageOS/android_hardware_google_graphics_common -b lineage-21.0 hardware/google/graphics/common
